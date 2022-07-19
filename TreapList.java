5902436685253853bb83bee5ee0b3064339d4127
samt12589 <84978134+samt12589@users.noreply.github.com>
2022-07-18 21:56:35 -0400

    Update TreapList.java
    Added toString and a private method named inorder

--- TreapList.java
+++ TreapList.java
@@ -11,7 +11,7 @@ public class TreapList<E> implements List<E> {
     private E returndata;
 	
 	// A Treap Node
-	    private static class TreapNode<E>
+	private static class TreapNode<E>
 	{
 	    E data;
 	    int priority;
@@ -29,54 +29,13 @@ public class TreapList<E> implements List<E> {
 	}
 
 	   // TreapNode root;
-	    public TreapList()
+	public TreapList()
 	    {
 	        this.root=null;
             this.returndata=null;
-
 	    }
 
-	    /* Function to left-rotate a given treap
-	 
-	          r                         R
-	         / \      Left Rotate      / \
-	        L   R        ———>         r   Y
-	           / \                   / \
-	          X   Y                 L   X
-	    */
-	    private TreapNode<E> rotateLeft(TreapNode<E> root)
-	    {
-	        TreapNode<E> R = root.right;
-	        TreapNode<E> X = root.right.left;
-	 
-	        // rotate
-	        R.left = root;
-	        root.right = X;
-	 
-	        // set a new root
-	        return R;
-	    }
-	 
-	    /* Function to right-rotate a given treap
-	 
-	            r                        L
-	           / \     Right Rotate     / \
-	          L   R        ———>        X   r
-	         / \                          / \
-	        X   Y                        Y   R
-	    */
-	    private TreapNode<E> rotateRight(TreapNode<E> root)
-	    {
-	        TreapNode<E> L = root.left;
-	        TreapNode<E> Y = root.left.right;
-	 
-	        // rotate
-	        L.right = root;
-	        root.left = Y;
-	 
-	        // set a new root
-	        return L;
-	    }
+	   
 	 
 	    public boolean add(E e)
 	    {
@@ -86,85 +45,31 @@ public class TreapList<E> implements List<E> {
 	    
 	    public void add(int index, E e)
 	    {
-	        TreapNode<E> currroot=add(this.root, index, e);
+	        TreapNode<E> currroot=addNode(this.root, index, e);
 	        this.root =currroot ;
 	    }
-	     // Recursive function to insert a given key with a priority into treap
-	     //returns the base of the tree after completion
-	     private TreapNode<E> add(TreapNode<E> root, int index, E data)
-	    {
-	        // base case
-	        if (root == null) {
-	            return new TreapNode<E>(data);
-	        }
-	 
-	        // if data is less than the root node, insert in the left subtree;
-	        // otherwise, insert in the right subtree
-	        if(root.left!=null){
-	        if (index<= root.left.treecount)
-	        {
-	            root.left = add(root.left, index, data);
-	 
-	            // rotate right if heap property is violated
-	             if (root.left != null && root.left.priority > root.priority) {
-	                root = rotateRight(root);
-	            }
-
-	        }
-	        else{
-	            index=index-root.left.treecount;
-	        }
-	        
-	    }
-	    else if(index==0){
-	        root.left = add(root.left, index, data);
-
-	    }
-
-
-	        else {
-	            root.right = add(root.right, index-1, data);
-	 
-	            // rotate left if heap property is violated
-	            if (root.right != null && root.right.priority > root.priority) {
-	                root = rotateLeft(root);
-	            }
-	        }
-	        root.treecount++;
-	        return root;
-	    }
-	     
-	     
-	     
+	    
+	    
 	    // Recursive function to search for a key in a given treap
 	    public E get(int index){
 	        return searchNode(this.root, index );
 	    }
-	    private E searchNode(TreapNode<E> root, int key)
+	    
+	    
+	    
+	public int size()
 	    {
-	        // if the key is not present in the tree
-	        if (root == null) 
-	        {
-	            throw new IndexOutOfBoundsException();
-	        }
-	 
-	        // if the key is found	 
-	        // if the key is less than the root node, search in the left subtree
-	        if (root.left!=null){
-	        if(key < root.left.treecount) {
-	            return searchNode(root.left, key);
-	        }
-	        key-=root.left.treecount;
+	       if(this.root!=null) 
+	       {
+	    	 return this.root.treecount;
+	       }
+		   return 0;
 	    }
-	        if (key==0) {
-	            return root.data;
-	        } 
-	        // otherwise, search in the right subtree
-	        return searchNode(root.right, key-1);
-	    }
+	        
 	 
 
-	public void clear(){
+	public void clear()
+	{
 	    this.root=null;
 	}
 	    
@@ -179,7 +84,109 @@ public class TreapList<E> implements List<E> {
         return data;
 		//return null;
 	}
-	    
+	
+	
+	
+	public Iterator<E> iterator() {
+		
+		
+		return null;
+	}
+	
+	
+	
+	@Override
+	public String toString()
+		{   if (size() == 0) return "[]";
+			StringBuilder sb = new StringBuilder();
+			sb.append('[');
+			sb.append(inorder(this.root, sb));
+			sb.append(']');
+			
+			return sb.toString();
+		}
+	
+	
+	
+	
+	/*
+	 * -----------------------------------------------------------------------------------
+	 * Private methods for helping
+	 */
+	
+	
+	private TreapNode<E> addNode(TreapNode<E> root, int index, E data)
+    {
+        // base case
+        if (root == null) {
+            return new TreapNode<E>(data);
+        }
+ 
+        // if data is less than the root node, insert in the left subtree;
+        // otherwise, insert in the right subtree
+        if(root.left!=null){
+        if (index<= root.left.treecount)
+        {
+            root.left = addNode(root.left, index, data);
+ 
+            // rotate right if heap property is violated
+             if (root.left != null && root.left.priority > root.priority) {
+                root = rotateRight(root);
+            }
+
+        }
+        else{
+            index=index-root.left.treecount;
+        }
+        
+    }
+    else if(index==0){
+        root.left = addNode(root.left, index, data);
+
+    }
+
+
+        else {
+            root.right = addNode(root.right, index-1, data);
+ 
+            // rotate left if heap property is violated
+            if (root.right != null && root.right.priority > root.priority) {
+                root = rotateLeft(root);
+            }
+        }
+        root.treecount++;
+        return root;
+    }
+	
+	
+	
+	private E searchNode(TreapNode<E> root, int key)
+    {
+        // if the key is not present in the tree
+        if (root == null) 
+        {
+            throw new IndexOutOfBoundsException();
+        }
+ 
+        // if the key is found	 
+        // if the key is less than the root node, search in the left subtree
+        if (root.left!=null){
+        if(key < root.left.treecount) {
+            return searchNode(root.left, key);
+        }
+        key-=root.left.treecount;
+    }
+        if (key==0) {
+            return root.data;
+        } 
+        // otherwise, search in the right subtree
+        return searchNode(root.right, key-1);
+    }
+	  
+	
+	
+	
+	
 	private  TreapNode<E> deleteNode(TreapNode<E> root, int key)
 	    {
 	        if (root == null) {
@@ -254,109 +261,110 @@ public class TreapList<E> implements List<E> {
 	    
 	    
 	    
-	    public int size()
-	    {
-	       if(this.root!=null) return this.root.treecount;
-		   return 0;
-	    }
-	    
+	   
 	    
 	 
 	    // Utility function to print two-dimensional view of a treap using
 	    // reverse inorder traversal
-	    public String printTreap(TreapNode<E> root, int space)
+//	    public String printTreap(TreapNode<E> root, int space)
+//	    {
+//	        final int height = 10;
+//	        String returnvalue=null;
+//	 
+//	        // Base case
+//	        if (root == null) 
+//	        {
+//	            return returnvalue;
+//	        }
+//	 
+//	        // increase distance between levels
+//	        space += height;
+//	 
+//	        // print the left child first
+//	        returnvalue+=printTreap(root.left, space);
+//	        System.lineSeparator();
+//	 
+//	        // print the current node after padding with spaces
+//	        for (int i = height; i < space; i++) 
+//	        {
+//	            returnvalue+=", ";
+//	        }
+//	 
+//	        returnvalue+=(root.data + "[" + root.priority + "]\n");
+//	 
+//	        // print the left child
+//	        System.lineSeparator();
+//	        returnvalue+=printTreap(root.right, space);
+//	        return returnvalue;
+//	    }
+
+	
+
+	    private TreapNode<E> rotateLeft(TreapNode<E> root)
 	    {
-	        final int height = 10;
-	        String returnvalue=null;
+	        TreapNode<E> R = root.right;
+	        TreapNode<E> X = root.right.left;
 	 
-	        // Base case
-	        if (root == null) 
-	        {
-	            return returnvalue;
-	        }
+	        // rotate
+	        R.left = root;
+	        root.right = X;
 	 
-	        // increase distance between levels
-	        space += height;
-	 
-	        // print the left child first
-	        returnvalue+=printTreap(root.left, space);
-	        System.lineSeparator();
-	 
-	        // print the current node after padding with spaces
-	        for (int i = height; i < space; i++) 
-	        {
-	            returnvalue+=", ";
-	        }
-	 
-	        returnvalue+=(root.data + "[" + root.priority + "]\n");
-	 
-	        // print the left child
-	        System.lineSeparator();
-	        returnvalue+=printTreap(root.right, space);
-	        return returnvalue;
+	        // set a new root
+	        return R;
 	    }
+	
+	    /* Function to left-rotate a given treap
+		 
+        r                         R
+       / \      Left Rotate      / \
+      L   R        ———>         r   Y
+         / \                   / \
+        X   Y                 L   X
+  */
+  
 
-	 /** 
-	    public static void main(String[] args)
-	    {
-	        // Treap keys
-	        int[] keys = { 5, 2, 1, 4, 9, 8, 10 };
-	 
-	        // construct a treap
-	        TreapNode root = null;
-	        for (int key: keys) {
-	            root = insertNode(root, key);
-	        }
-	 
-	        System.out.println("Constructed treap:\n\n");
-	        printTreap(root, 0);
-	 
-	        System.out.println("\nDeleting node 1:\n\n");
-	        root = deleteNode(root, 1);
-	        printTreap(root, 0);
-	 
-	        System.out.println("\nDeleting node 5:\n\n");
-	        root = deleteNode(root, 5);
-	        printTreap(root, 0);
-	 
-	        System.out.println("\nDeleting node 9:\n\n");
-	        root = deleteNode(root, 9);
-	        printTreap(root, 0);
-	    }*/
-   
+  /* Function to right-rotate a given treap
+
+          r                        L
+         / \     Right Rotate     / \
+        L   R        ———>        X   r
+       / \                          / \
+      X   Y                        Y   R
+  */
+  private TreapNode<E> rotateRight(TreapNode<E> root)
+  {
+      TreapNode<E> L = root.left;
+      TreapNode<E> Y = root.left.right;
+
+      // rotate
+      L.right = root;
+      root.left = Y;
+
+      // set a new root
+      return L;
+  }
+  
+  
+  private StringBuilder inorder(TreapNode<E> root, StringBuilder sb) 
+	{     
+      if(root == null) 
+      {
+          return sb;
+      }
+      
+      inorder(root.left, sb);
+      sb.append(root.data).append(", ");
+      inorder(root.right, sb);
+      
+      return sb;
+	}
+	
+	/*
+	 * ------------------------------------------------------------------------------------------
+	 * End of Private methods
+	 */
 	
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 
 	@Override
@@ -365,12 +373,7 @@ public class TreapList<E> implements List<E> {
 	     return false;
 	}
 	
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		
-		return null;
-	}
+	
 
 
 	@Override
@@ -387,12 +390,10 @@ public class TreapList<E> implements List<E> {
 	}
 
 	@Override
-	public Object[] toArray(Object[] a) {
+	public <T> T[] toArray(T[] a) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 
 	@Override
 	public boolean remove(Object o) {
@@ -401,31 +402,31 @@ public class TreapList<E> implements List<E> {
 	}
 
 	@Override
-	public boolean containsAll(Collection c) {
+	public boolean containsAll(Collection<?> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean addAll(Collection c) {
+	public boolean addAll(Collection<? extends E> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean addAll(int index, Collection c) {
+	public boolean addAll(int index, Collection<? extends E> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean removeAll(Collection c) {
+	public boolean removeAll(Collection<?> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean retainAll(Collection c) {
+	public boolean retainAll(Collection<?> c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -433,17 +434,10 @@ public class TreapList<E> implements List<E> {
 	
 
 	@Override
-	public Object set(int index, Object element) {
+	public E set(int index, E element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-//	@Override
-//	public void add(int index, Object element) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-
 
 	@Override
 	public int indexOf(Object o) {
@@ -458,19 +452,19 @@ public class TreapList<E> implements List<E> {
 	}
 
 	@Override
-	public ListIterator listIterator() {
+	public ListIterator<E> listIterator() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public ListIterator listIterator(int index) {
+	public ListIterator<E> listIterator(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List subList(int fromIndex, int toIndex) {
+	public List<E> subList(int fromIndex, int toIndex) {
 		// TODO Auto-generated method stub
 		return null;
 	}
